4761d8d3880ff5be3979e1a7cb2b420ad2f0b934
TSTP <86605720+seokjin0404@users.noreply.github.com>
2022-10-07 11:36:16 +0900

    feat 0단계 StringTest 실습

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,37 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+    @Test
+    void split() {
+        String[] numbers = "1,2".split(",");
+        assertThat(numbers).containsExactly("1","2");
+        numbers = "1".split(",");
+        assertThat(numbers).contains("1");
+    }
+    @Test
+    void substring() {
+        String numbers = "(1,2)".substring(1,4);
+        assertThat(numbers).isEqualTo("1,2");
+    }
+    @Test
+    @DisplayName("Test StringIndexOutOfBoundsException")
+    void charAt() {
+        String values = "abc";
+        int input = 4;
+
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    char chr = values.charAt(input);
+                });
     }
 }
