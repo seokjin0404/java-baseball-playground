b0d1504e376cb5cf5fc267999820d59281b69014
TSTP <86605720+seokjin0404@users.noreply.github.com>
2022-10-08 10:24:28 +0900

    feat 1단계 CalculateTest 작성

--- src/test/java/study/CalculateTest.java
+++ src/test/java/study/CalculateTest.java
@@ -1,13 +1,24 @@
 package study;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CalculateTest {
 
+    private Calculator cal;
+    @BeforeEach
+    void setUp() {
+        cal = new Calculator();
+    }
+
     @Test
-    @DisplayName("calculate")
-    void calculate() {
-        System.out.println(3);
+    @DisplayName("문자열 계산기")
+    void calculateTest() {
+        String input = "2 + 3 * 4 / 2";
+        int result = cal.calculate(input);
+        assertThat(result).isEqualTo(10);
     }
 }
